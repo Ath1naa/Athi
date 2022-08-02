450f785e9be165511aef37ee2f1e84ced178df91
Ath <Ath@DESKTOP-BKIKV5C>
2022-08-02 08:49:27 -0300

    objetos

---
+++ src/grupo3java/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo3java;
+
+/**
+ *
+ * @author Ath
+ */
+public class Persona {
+    private string nombre;
+    private int edad;
+    private float dni;
+    private boolean sexo;
+    private float peso;
+    private float altura;
+    
+}
